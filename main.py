4897a8fa7c6acdd814d0b9ed50058c8d256bc311
#Eduh <emwiti658@gmail.com>
2023-12-31 18:42:23 +0300

    adds matplotlib tests

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter
 from tkinter import *
+from tkinter import filedialog as fd
+import csv
 
 frame_config = {
     "relief": GROOVE,
@@ -9,77 +11,101 @@ frame_config = {
 file_entry_config = {
     "width": 30,
     "relief": RAISED,
-    "font": ('Arial', 13)
+    "font": ('Verdana', 10)
 }
 
 # plot option buttons
 plot_buttons = ["PLOT RAW", "PLOT FILTERED", "COMBINE"]
 plot_btns_config = {
     "relief": RAISED,
-    "font": ("Arial", 10, "bold")
+    "font": ("Verdana", 10, "bold")
 }
 
-def init_widgets():
-    '''
-    Initialize widgets for the app
-    '''
 
-    # parent frame
-    root_frame = LabelFrame(root, background="red")
-    root_frame.pack(side="left", fill=Y, ipadx=10, ipady=20, pady=10)
+# file operations variables
+# functions
+def open_raw_file():
+    """
+    open raw data file, csv, split it plot
+    :return:
+    """
 
-    graph_frame = LabelFrame(root, text="test", background="blue")
-    graph_frame.pack(side="left", expand=True)
+    file_types = (
+        ('csv files', '*.csv'),
+        ('text files', '*.txt')
+    )
 
-    l = Label(graph_frame, text="Click")
-    l.pack(side="right")
+    data_file = fd.askopenfilename(
+        title='Choose file',
+        filetypes=file_types
+    )
 
-    # create file select frame
-    file_frame = LabelFrame(root_frame, text="Select files")
-    file_frame.pack(ipadx=10, side="top", pady=15)
+    with open(data_file, 'r') as file:
+        reader = csv.reader(file, delimiter=",")
+        for row in reader:
+            print(row)
 
-    raw_file_frame = Frame(file_frame)
-    raw_file_frame.pack(side="top")
-    raw_file_label = Label(raw_file_frame, text="Raw file")
-    raw_file_label.pack(side="left")
-    raw_file_entry = Entry(raw_file_frame, file_entry_config)
-    raw_file_entry.pack(side="left")
-    raw_file_select_btn = Button(raw_file_frame, text="...")
-    raw_file_select_btn.pack(side="left")
+    # update GUI
+    raw_file_entry.insert(0, data_file)
 
-    filter_file_frame = Frame(file_frame)
-    filter_file_frame.pack(side="top")
-    filter_file_label = Label(filter_file_frame, text="Filter file")
-    filter_file_label.pack(side="left")
-    filter_file_entry = Entry(filter_file_frame, file_entry_config)
-    filter_file_entry.pack(side="left")
-    filter_file_select_btn = Button(filter_file_frame, text="...")
-    filter_file_select_btn.pack(side="left")
+    # show file opening operations
+    raw_file_open_status = Label(info_frame, text="[ ] Raw file loaded. Ready to plot...")
+    raw_file_open_status.pack(anchor="w")
 
-    # create plotting buttons
-    plot_btn_frame = LabelFrame(root_frame, frame_config)
-    plot_btn_frame.pack(side="top", pady=15, padx=15, fill=X)
 
-    plot_raw_btn = Button(plot_btn_frame, plot_btns_config, text=plot_buttons[0])
-    plot_raw_btn.pack(side="left", expand=True)
-    plot_filtered_btn = Button(plot_btn_frame, plot_btns_config, text=plot_buttons[1])
-    plot_filtered_btn.pack(side="left", expand=True)
-    combine_plots_btn = Button(plot_btn_frame, plot_btns_config, text=plot_buttons[2])
-    combine_plots_btn.pack(side="left", expand=True)
-
-    # info frame
-    info_frame = LabelFrame(root_frame, frame_config, text="Info")
-    info_frame.pack(side="top", anchor="w", padx=15, pady=15, expand=True, fill=BOTH)
-
-    info_frame_entry = Label(info_frame, font=("Verdana", 10), text="Raw file opening failed")
-    info_frame_entry.pack(side="left") # TODO: make this dynamic
-
+"""
+Initialize widgets for the app
+"""
 
 root = Tk()
 root.geometry('1400x700')
 root.title("Plottar")
 
-# create widgets
-init_widgets()
+# parent frame
+root_frame = LabelFrame(root)
+root_frame.pack(side="left", fill=Y, ipadx=10, ipady=20, pady=10)
+
+graph_frame = LabelFrame(root, text="test")
+graph_frame.pack(side="left", expand=True)
+
+l = Label(graph_frame, text="Click")
+l.pack(side="right")
+
+# create file select frame
+file_frame = LabelFrame(root_frame, text="Select files")
+file_frame.pack(ipadx=10, side="top", pady=15)
+
+raw_file_frame = Frame(file_frame)
+raw_file_frame.pack(side="top")
+raw_file_label = Label(raw_file_frame, text="Raw file")
+raw_file_label.pack(side="left")
+raw_file_entry = Entry(raw_file_frame, file_entry_config)
+raw_file_entry.pack(side="left")
+raw_file_select_btn = Button(raw_file_frame, text="...", command=open_raw_file)
+raw_file_select_btn.pack(side="left")
+
+filter_file_frame = Frame(file_frame)
+filter_file_frame.pack(side="top")
+filter_file_label = Label(filter_file_frame, text="Filter file")
+filter_file_label.pack(side="left")
+filter_file_entry = Entry(filter_file_frame, file_entry_config)
+filter_file_entry.pack(side="left")
+filter_file_select_btn = Button(filter_file_frame, text="...")
+filter_file_select_btn.pack(side="left")
+
+# create plotting buttons
+plot_btn_frame = LabelFrame(root_frame, frame_config)
+plot_btn_frame.pack(side="top", pady=15, padx=15, fill=X, ipady=5)
+
+plot_raw_btn = Button(plot_btn_frame, plot_btns_config, text=plot_buttons[0])
+plot_raw_btn.pack(side="left", expand=True)
+plot_filtered_btn = Button(plot_btn_frame, plot_btns_config, text=plot_buttons[1])
+plot_filtered_btn.pack(side="left", expand=True)
+combine_plots_btn = Button(plot_btn_frame, plot_btns_config, text=plot_buttons[2])
+combine_plots_btn.pack(side="left", expand=True)
+
+# info frame
+info_frame = LabelFrame(root_frame, frame_config, text="Info")
+info_frame.pack(side="left", anchor="w", padx=15, pady=15, expand=True, fill=BOTH)
 
 mainloop()
---
+++ plot-tests/first.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# creating an ndarray
+# x = np.array([1,2,3])
+# y = np.array([4, 5, 6])
+
+# multidimensional array
+# m= np.array([ [1, 2, 3], [4, 5, 6] ])
+# print(m[1,2])
+
+# print(x+y)
+x = np.arange(0.0, 6.0, 0.01)
+plt.plot(x, [xi**2 for xi in x], label="test1")
+plt.plot(x, [xi*3 for xi in x], label="test2")
+plt.plot(x, [xi*1.5 for xi in x], label="test3")
+
+# enable legend visibility
+plt.legend(loc='upper left')
+
+# show grid
+plt.grid(True)
+
+# handle axis
+# show current axis limits
+plt.axis(xmin=-1, xmax=8, ymin=-2, ymax=40)
+
+# set axes labels
+plt.xlabel('No. of samples')
+plt.ylabel('value')
+
+# set plot title
+plt.title("Test data")
+
+# save a plot to file
+plt.savefig('plt1.png')
+
+plt.show()
